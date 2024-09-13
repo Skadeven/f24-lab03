3ac0d952eef45b0b0661e5affc1b69d9040ae6db
Skadeven <jshi4@andrew.cmu.edu>
2024-09-13 12:03:08 -0400

    Implemented Instrumentation with delegation in java, passed all tests

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -12,5 +12,78 @@ package edu.cmu.cs.cs214.rec04;
 
 public class DelegationSortedIntList extends SortedIntList{
     // Write your implementation below with API documentation
+    private final SortedIntList sortedList;
+    private int totalAdded;
+    // Write your implementation below with API documentation
+    /**
+     * Adds the specified int to the list.
+     *
+     * @param num an integer to be added to the list
+     * @return true if the list is changed as a result of the call
+     */
+
+    public DelegationSortedIntList() {
+        sortedList = new SortedIntList(); 
+        this.totalAdded = 0;
+    }
+
+    @Override
+    public boolean add(int num){
+        totalAdded++; 
+        return sortedList.add(num);
+    }
+
+    /**
+     * Adds all of the elements of the IntegerList to the list.
+     *
+     * @param list IntegerList containing elements to be added to the list
+     * @return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean addAll(IntegerList list){
+        boolean result = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (add(list.get(i))) { // Use add method for each element
+                result = true;
+            }
+        }
+        return result;
+    }
+
+
+    // Return the total number of added elements
+    public int getTotalAdded() {
+    return totalAdded;
+    }
+
+    // Delegate the get method to SortedIntList
+    @Override
+    public int get(int index) {
+        return sortedList.get(index);
+    }
+
+    // Delegate the remove method to SortedIntList
+    @Override
+    public boolean remove(int num) {
+        return sortedList.remove(num);
+    }
+
+    // Implement the removeAll method using IntegerList for delegation
+    @Override
+    public boolean removeAll(IntegerList list) {
+        boolean modified = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (remove(list.get(i))) { // Use remove method for each element
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    // Delegate the size method to SortedIntList
+    @Override
+    public int size() {
+        return sortedList.size();
+    }
 
 }
